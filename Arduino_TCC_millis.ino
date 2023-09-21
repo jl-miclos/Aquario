1168340c606f7a5a988090f5abf87b9e30094281
jl-miclos <122179319+jl-miclos@users.noreply.github.com>
2023-09-21 14:01:48 -0300

    Add files via upload
    Código Arduino para controle de sistema de aquário automatizado, com controle de temperatura, luminosidade, alimentação e troca de água. 
    Código Esp8266 para integração e controle de Arduino via app
    Código Aplicativo para controle remoto de ESP8266 através da internet

---
+++ Arduino_TCC_millis.ino
@@ -0,0 +1,122 @@
+#include <Stepper.h>
+
+unsigned long millisIluminacao = millis();
+const unsigned long IntervaloV = 10;
+unsigned long tempV= 0;
+const unsigned long IntervaloA= 10;
+unsigned long tempA= 0;
+String msg;
+int Aquecedor = 4; //Mudar para relé do aquecedor
+int Cooler = 5; //Mudar para relé do cooler
+int FV = 2; //Fita de led vermelho
+int FA = 3; //Fita de led azul
+int SensorSump = 22; //Sensor acima do de seguranca
+int SensorSeguranca = 24;  //Sensor mais de baixo do SUMP
+int SensorReservatorio = 26; //Sensor do tanque de agua limpa
+int BombaSuja = 28; //Bomba que sai do sump para tanque, utiliza so na TPA
+int BombaLimpa = 30; //Bomba que sai do tanque para sump, utiliza so na TPA
+int BombaSump = 32; //Bomba NF, so abre o rele no durante a TPA
+const int stepsPerRevolution = 2050; //Passos em 360
+const int stepsPerRevolution2 = 1281; //Passos em 225
+const int stepsPerRevolution3 = 1025; //Passos em 180
+const int stepsPerRevolution4 = 512; //Passos em 90
+const int stepsPerRevolution5 = 256; //Passos em 45
+Stepper myStepper(stepsPerRevolution, 6, 8, 7, 9); 
+
+void setup() {
+Serial.begin(115200);
+Serial3.begin(115200); 
+myStepper.setSpeed(4); //Numero de rotações por minuto, SE TIVER LENTO VOCE AUMENTA ATE 60
+pinMode(Aquecedor, OUTPUT);
+pinMode(Cooler, OUTPUT);
+pinMode(FV, OUTPUT);
+pinMode(FA, OUTPUT);
+pinMode(SensorSump, OUTPUT);
+pinMode(SensorSeguranca, OUTPUT);
+pinMode(SensorReservatorio, OUTPUT);
+pinMode(BombaSuja, OUTPUT);
+pinMode(BombaLimpa, OUTPUT);
+pinMode(BombaSump, OUTPUT);   
+}
+
+void loop() {
+
+if (Serial3.available()) {  //Aguarde dados vindos do ESP8266.
+char data = Serial3.read();  //Leitura de um byte.
+Serial.print(data);  //Imprima o mesmo dado pela porta usb.
+msg += data;  //Acrescente o caractere recebido a string de mensagem.
+if (data == 13) {  //Limpa a string ao receber caractere CR(Carriage return).
+msg = "";
+}  
+Iluminacao();
+
+if (msg.indexOf("LC") > 0) { //Receber ordem do ESP8266
+digitalWrite(Cooler, HIGH); 
+digitalWrite(Aquecedor, LOW); 
+}
+else if (msg.indexOf("LA") > 0) { //Receber ordem do ESP8266
+digitalWrite(Aquecedor, HIGH); 
+digitalWrite(Cooler, LOW); 
+}
+else if(msg.indexOf("LN") > 0) { //Receber ordem do ESP8266
+digitalWrite(Aquecedor, LOW);
+digitalWrite(Cooler, LOW); 
+}
+
+if (msg.indexOf("IA") > 0) { //Receber ordem do ESP8266 
+myStepper.step(stepsPerRevolution3);
+myStepper.step(stepsPerRevolution5);
+myStepper.step(-stepsPerRevolution4);
+myStepper.step(stepsPerRevolution2);
+}
+
+//ARRUMAR COM A LÓGICA QUE OS SENSORES ESTIVEREM NA CASA DO JOÃO
+if (msg.indexOf("ITPA") > 0) { //Receber ordem do ESP8266   
+if(digitalRead(SensorSeguranca) == HIGH){ //Se for high, executa todo o processo 
+  
+if(digitalRead(SensorSump) == HIGH && digitalRead(SensorReservatorio) == HIGH){
+digitalWrite(BombaSump, HIGH); 
+digitalWrite(BombaSuja, HIGH);
+}
+else if(digitalRead(SensorSump) == LOW && digitalRead(SensorReservatorio) == HIGH){
+digitalWrite(BombaLimpa, HIGH);
+digitalWrite(BombaSuja, LOW);
+}
+else if(digitalRead(SensorSeguranca) == HIGH && digitalRead(SensorReservatorio) == LOW){
+digitalWrite(BombaSump, LOW);  
+digitalWrite(BombaLimpa, LOW);
+}
+}
+else if(digitalRead(SensorSeguranca) == LOW){ //Se for low, para tudo!(com a intenção de não queimar as bombas)
+digitalWrite(BombaSuja, LOW);
+digitalWrite(BombaLimpa, LOW);
+digitalWrite(BombaSump, LOW);
+}}}}
+
+
+void Iluminacao(){
+    
+if (msg.indexOf("IL") > 0) { //Receber ordem do ESP8266
+int x = 1; 
+millisIluminacao = tempV;
+if (millisIluminacao- tempV >= IntervaloV)
+{
+for (int i = 0; i > -1; i = i + x) {
+analogWrite(FV, i);
+if (i == 255) { //led no maximo
+x = -1; }
+millisIluminacao = tempV;} //ALTERAR ESSES VALORES
+}
+int y = 1; 
+millisIluminacao = tempA;
+if (millisIluminacao - tempA >= IntervaloA)
+{
+for (int i = 0; i > -1; i = i + y) {
+analogWrite(FA, i);
+if (i == 255) {
+y = -1;
+}
+} //ALTERAR ESSES VALORES
+millisIluminacao = tempA;}
+ }
+}
---
+++ Esp_TCC.ino
@@ -0,0 +1,135 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Wire.h>
+#include<LiquidCrystal_I2C.h>
+#include <ESP8266WiFi.h>
+
+IPAddress ip(192, 168, 0, 100);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+IPAddress dns(192, 168, 0, 1);
+
+const char* ssid = "Miclos"; //Nome da rede 
+const char* password = "04122000"; //Senha da rede
+WiFiServer server(80); 
+
+LiquidCrystal_I2C lcd(0x27,16,2); 
+OneWire onewire(0); // GPIO where the DS18B20 is connected to
+DallasTemperature TemperaturaIdeal(&onewire);
+float temperatura;
+
+void setup() {
+Wire.begin(4, 5); //SDA = 4; SCL = 5.
+lcd.init();
+TemperaturaIdeal.begin();
+Serial.begin(115200);
+delay(10);
+Serial.println(); // Connect to WiFi network
+Serial.println();
+Serial.print("Conectando ");
+Serial.println(ssid);
+
+WiFi.config(ip, dns, gateway, subnet);
+
+WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi conectado");
+Serial.println(WiFi.localIP());
+server.begin();
+}
+
+void loop() {
+   
+TemperaturaIdeal.requestTemperatures(); 
+temperatura = TemperaturaIdeal.getTempCByIndex(0);
+lcd.setBacklight(HIGH);
+lcd.setCursor(0,0);
+lcd.print("  Temperatura:");
+delay(100);
+lcd.setCursor(5,1);
+lcd.print(temperatura);
+delay(100);
+ 
+WiFiClient client = server.available();  // Check if a client has connected
+if (!client) { //Se o cliente não se conectar, voltar até ele se conectar
+return; 
+}
+while(!client.available()){
+delay(1);
+}
+String req = client.readStringUntil('\r');
+client.flush();
+
+if (req.indexOf("FT1") >= 0) {  
+ if(temperatura> 23) {
+ Serial.println("LC");
+ }
+ else if(temperatura< 21) {
+ Serial.println("LA");
+ }
+ else if(temperatura>= 21 && temperatura<= 23){
+ Serial.println("LN");
+}}  
+ 
+if (req.indexOf("FT2") >= 0) {  
+ if(temperatura> 25) {
+ Serial.println("LC");
+ }
+ else if(temperatura< 23) {
+ Serial.println("LA");
+ }
+ else if(temperatura>= 23 && temperatura<= 25){
+ Serial.println("LN");
+}}
+ 
+if (req.indexOf("FT3") >= 0) {
+ if(temperatura> 27) {
+ Serial.println("LC");
+ }
+ else if(temperatura< 25) {
+ Serial.println("LA");
+ }
+ else if(temperatura>= 25 && temperatura<= 27){
+ Serial.println("LN");
+}}
+ 
+if (req.indexOf("FT4") >= 0) {   
+ if(temperatura> 29) {
+ Serial.println("LC");
+ }
+ else if(temperatura< 27) {
+ Serial.println("LA");
+}
+ else if(temperatura>= 27 && temperatura<= 29){
+ Serial.println("LN");
+}}
+ 
+if (req.indexOf("FT5") >= 0) {    
+ if(temperatura> 31) {
+ Serial.println("LC");
+ }
+ else if(temperatura< 29) {
+ Serial.println("LA");
+}
+ else if(temperatura>= 29 && temperatura<= 31){
+ Serial.println("LN");
+}}
+
+if (req.indexOf("FL") >=0) {  //FITA LED
+Serial.println("IL");
+}
+  
+if (req.indexOf("PA") >=0) {  //ALIMENTADOR
+Serial.println("IA");
+}
+
+if (req.indexOf("TA") >=0) {  //TPA
+Serial.println("ITPA");
+}
+client.flush();
+delay(1);
+}
